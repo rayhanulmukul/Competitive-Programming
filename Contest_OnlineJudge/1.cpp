b8ebcedd318d326ea66987566511f51a1183c8c6
Md Rayhanul Islam <73143090+rayhanul2008@users.noreply.github.com>
2023-02-06 19:47:33 +0600

    update

--- Contest_OnlineJudge/1.cpp
+++ Contest_OnlineJudge/1.cpp
@@ -1,69 +1,40 @@
-#include <bits/stdc++.h>
-#include<vector>
+#include<bits/stdc++.h>
 using namespace std;
-
-#define ll long long int
-#define int ll
-#define ld long double
-#define pb push_back
-#define mp make_pair
-#define ft front()
-#define bk back()
-#define pi 2*acos(0.0)     /// acos(-1) , 3.14159265359
-#define gap ' '
-#define en '\n'
-#define endl en
-#define sz(x) (int(x.size()))
-#define mem(a, b) memset(a, b, sizeof(a))
-#define sor(x)  sort(x.begin(), x.end())
- 
-mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
-#define rng(x,y) uniform_int_distribution<int>(x,y)(rng)
-#define F0R(i,a,b) for (int i = (a); i < (b); ++i)
-#define FOR(i,a) F0R(i,0,a)
-#define R0F(i,a,b) for (int i = (b)-1; i >= (a); --i)
-#define ROF(i,a) R0F(i,0,a)
-#define each(a,x) for (auto& a: x)
-const int MOD = 1e9+7; // 998244353;
-const int MAX = 2e5+5;
-const int N = 1005;
-
-void solve(int tt){
-    int n;  
-    cin >> n;
+int main()
+{
+   int t;
+   cin>>t;
+   while(t--)
+   {
+    int n;
+    cin>>n;
     string s;
-    cin >> s;
-    int x = 0, y = 0;
-    bool ok = 0;
-    int i = 0;
-    while (n--){
-        if(x == 1 && y == 1){
-            ok = 1;
-            break;
-        }
-        if(s[i] == 'L') x--;
-        else if(s[i] == 'R') x++;
-        else if(s[i] == 'U') y++;
-        else y--;
-        i++;
-        //cout << x << gap << y << endl;
+    cin>>s;
+    map<char,int>mp;
+    set<char>s1;
+    for(int i=0;i<s.length();i++)
+    {
+      mp[s[i]]++;
+      s1.insert(s[i]);
+    }
+     int p=s1.size();
+     set<char>s2;
+     int mx=0;
+     int temp;
+     for(int i=0;i<s.length();i++)
+     {
+      mp[s[i]]--;
+      if(mp[s[i]]==0)
+       p--;
+      
+      s2.insert(s[i]);
+      
+      temp=s2.size();
+        mx=max(mx,(temp+p));
+   
+     }
 
-    }
-    if(x == 1 && y == 1){
-            ok = 1;
-    }
-    if(ok) cout << "YES" << endl;
-    else cout << "NO" << endl;
-}
-int32_t main(){
-    #ifndef DEBUG
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-    #endif
-    int t = 1;
-    cin >> t;
-    for(int i = 1; i <= t; i++){
-        solve(i);
-    }
-    return 0;
+     cout<<mx<<endl;
+   }
+
 }
--- Contest_OnlineJudge/2.cpp
+++ Contest_OnlineJudge/2.cpp
@@ -1,19 +1,76 @@
-#include <iostream>
-#include <string>
-#include <algorithm>
-
+#include <bits/stdc++.h>
+#include<vector>
 using namespace std;
 
-int main()
-{
-    string input;
-    getline(cin, input);
+#define ll long long int
+#define int ll
+#define ld long double
+#define pb push_back
+#define mp make_pair
+#define ft front()
+#define bk back()
+#define pi 2*acos(0.0)     /// acos(-1) , 3.14159265359
+#define gap ' '
+#define en '\n'
+#define endl en
+#define sz(x) (int(x.size()))
+#define mem(a, b) memset(a, b, sizeof(a))
+#define sor(x)  sort(x.begin(), x.end())
+ 
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+#define rng(x,y) uniform_int_distribution<int>(x,y)(rng)
+#define F0R(i,a,b) for (int i = (a); i < (b); ++i)
+#define FOR(i,a) F0R(i,0,a)
+#define R0F(i,a,b) for (int i = (b)-1; i >= (a); --i)
+#define ROF(i,a) R0F(i,0,a)
+#define each(a,x) for (auto& a: x)
+const int MOD = 1e9+7; // 998244353;
+const int MAX = 2e5+5;
+const int N = 1005;
 
-    transform(input.begin(), input.end(), input.begin(), ::tolower);
-    if (input.find("magic") != string::npos)
-        cout << "Magic Exists!" << endl;
-    else
-        cout << "Magic Where :(" << endl;
+void solve(int tt){
+    int n, w, h, ans = 0;
+    cin >> n >> w >> h;
+    int arr[n];
+    int brr[n];
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    for(int i = 0; i < n; i++){
+        cin >> brr[i];
+    }
+    if(arr[0] < brr[0]){
+        int x = brr[0] - arr[0];
+        FOR(i, n){
+            brr[i] -=x;
+        }
+    }
+    else{
+        int x = arr[0] - brr[0];
+        FOR(i, n){
+            brr[i] +=x;
+        }
+    }
+    FOR(i, n){
+        if(arr[i]-w <= brr[i]-h && brr[i]+h <= arr[i]+w) continue;
+        else {
+            cout << arr[i]-w << gap << brr[i]-h << gap << arr[i]+w << gap << brr[i]+h << en;
+            cout << "NO" << en;
+            //return;
+        }
 
+    }
+   // cout << "YES" << en;
+}
+int32_t main(){
+    #ifndef DEBUG
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+    #endif
+    int t = 1;
+    cin >> t;
+    for(int i = 1; i <= t; i++){
+        solve(i);
+    }
     return 0;
 }
