a5aacd60f19638ac350ed240e82e8d17141e5ea5
Md Rayhanul Islam <73143090+rayhanulislam1@users.noreply.github.com>
2024-08-01 02:31:19 +0600

    change

--- Contest_OnlineJudge/1.cpp
+++ Contest_OnlineJudge/1.cpp
@@ -1,126 +1,56 @@
-//ami pari na ar pari na
-#include<bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace std ;
+/*بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيمِ*/
+#include "ext/pb_ds/assoc_container.hpp"
+#include "ext/pb_ds/tree_policy.hpp"
+#include <bits/stdc++.h>
+using namespace std;
 using namespace __gnu_pbds;
-#define                pb push_back 
-#define                mp make_pair
-#define                in insert  
-#define                fi first
-#define                se second
-#define                ll long long 
-#define                ld long double
-#define                ull unsigned long long 
-#define                pi acos(-1)
-#define                test() int T,t=1;cin>>T;
-#define                all(x) (x).begin(),(x).end()
-#define                sz(x) (int)(x.size())
-#define                int    long long int
-#define bits(x)        __builtin_popcountll(x)
-#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
-              
+
+#define ll long long int
+#define int ll
+#define ld long double
+#define pb push_back
+#define ft front()
+#define bk back()
+#define pi 2*acos(0.0)
+#define gap ' '
+#define en '\n'
+#define endl en
+#define mem(a, b) memset(a, b, sizeof(a))
 #ifdef TESLA
 #include "main.hpp"
 #else
 #define dbg(...)
 #endif
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+#define rng(x,y) uniform_int_distribution<int>(x,y)(rng)
+#define F0R(i,a,b) for (int i = (a); i < (b); ++i)
+#define FOR(i,a) F0R(i,0,a)
+const int MOD = 1e9+7; // 998244353;
+const int MAX = 2e5+5;
+const int INF = 1e18;
+int dx[] = {0, 0, +1, -1, -1, +1, -1, +1};
+int dy[] = {+1, -1, 0, 0, +1, +1, -1, -1};
 
-const ll INF = 1e18 ;
-const int N = 3e5 + 10  ;
-
-// Global Variablea
-int dx[]={0, 0, 1, -1, 1, 1, -1, -1};
-int dy[]={1, -1, 0, 0, 1, -1, 1, -1};
-
-//priority_queue <int, vector<int>, greater<int>> pq;
-//---------------------------------------------------------------------------------------------
-bool sortbysec(const pair<int,int> &a,const pair<int,int> &b){return (a.second < b.second);}
-ll Ceil(ll dividend , ll divisor){return (dividend / divisor +  (dividend % divisor ? 1 : 0));}
-vector<ll> sieve(int n) {int*arr = new int[n + 1](); vector<ll> vect; for (int i = 2; i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (int j = 2 * i; j <= n; j += i)arr[j] = 1;} return vect;}
-long long binpow(long long a, long long b) { if (b == 0) return 1;long long res = binpow(a, b / 2);if (b % 2) return res * res * a; else return res * res;}
-int binpowMod(int a, int b , int mod ){ if(b == 0) return 1 ; int ans = 1 ;while(b > 0){if(b & 1) ans = (ans * a) % mod ;a = (a * a) % mod ;b = b >> 1;}return ans % mod;}
-//---------------------------------------------------------------------------------------------
-
-vector<vector<int>> graph;
-vector<int> visited;
-vector<int> firstTime;
-vector<int> lowTime;
-vector<int> cntNodes;
-int timer;
-int ans;
-int n, m;
-
-int getSum(int x){
-    int sum = x * (x - 1);
-    sum >>= 1ll;
-    return sum;
-}
-void dfs(int vertex, int parent){
-    visited[vertex] = 1;
-    firstTime[vertex] = lowTime[vertex] = ++timer;
-    for(auto child : graph[vertex]){
-        if(child == parent) continue;
-        if(visited[child] == 0){
-            dfs(child, vertex);
-            cntNodes[vertex] += cntNodes[child];
-
-            lowTime[vertex] = min(lowTime[vertex], lowTime[child]);
-            if(lowTime[child] > firstTime[vertex]){
-                int cc1 = cntNodes[child];
-                int cc2 = n - cc1;
-                int sum = getSum(cc1) + getSum(cc2);
-                ans = min(ans, sum);
-            }
-        }else{
-            lowTime[vertex] = min(lowTime[vertex], lowTime[child]);
-        }
+void solve(int tt){
+    int n;
+    cin >> n;
+    set <pair<string, string>> sp;
+    for(int i = 0; i < n; i++){
+        string s1, s2;
+        cin >> s1 >> s2;
+        sp.insert({s1, s2});
     }
-    cntNodes[vertex]++;
+    cout << sp.size() << en;
 }
-void clearAll(){
-    graph.clear();
-    visited.clear();
-    firstTime.clear();
-    lowTime.clear();
-    cntNodes.clear();
-}
-void solve(){
-    cin >> n >> m;
-
-    clearAll();
-    graph.resize(n + 1);
-    visited.resize(n + 1, 0ll);
-    firstTime.resize(n + 1);
-    lowTime.resize(n + 1);
-    cntNodes.resize(n + 1, 0ll);
-    timer = 0;
-    ans = INF;
-
-    for(int i = 0; i < m; i += 1){
-        int u, v;
-        cin >> u >> v;
-        graph[u].push_back(v);
-        graph[v].push_back(u);
+int32_t main(){
+#ifndef DEBUG
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+#endif
+    int t = 1;
+    //cin >> t;
+    for(int i = 1; i <= t; i++){
+        solve(i);
     }
-    // dbg(graph);
-    dfs(1, -1);
-    if(ans == INF) ans = getSum(n);
-    cout << ans << '\n';
+    return 0;
 }
-int32_t main(){   
-       ios_base::sync_with_stdio(false);cin.tie(NULL);
-       // freopen("input.txt" , "r" , stdin) ;
-       // freopen("output.txt" , "w" , stdout) ;
-       int testCase = 1 , T = 1;
-       cin >> T;            
-       while(testCase <= T){
-              //cout << "Case #" << testCase << ": ";
-              //cout << "Case " << testCase << ": " ;   
-              //cout << "Scenario #" << testCase << ": " ;
-              solve();
-              testCase++; 
-       } 
-
-       return 0; 
-}      
--- Contest_OnlineJudge/tempCodeRunnerFile.cpp
+++ Contest_OnlineJudge/tempCodeRunnerFile.cpp
@@ -1,12 +1,1 @@
-  for(int j = 0; j < m; j++){
-    //         if(j - 1 >= 0 && mat[i][j - 1] > mat[i][j] && mat[i][j] > mat[i][j + 1] && j + 1 < m){
-    //             mat[i][j] = min(mat[i][j - 1], mat[i][j + 1]);
-    //         }
-    //     }
-    // }
-    // for(int i = 0; i < n; i++){
-    //     for(int j = 0; j < m; j++){
-    //         cout << mat[i][j] << gap;
-    //     }
-    //     cout << en;
-    // }
+0
