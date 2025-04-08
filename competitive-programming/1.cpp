01e6cdc623b2e0bc5450c83ccabaf19e2c96b76b
Md Rayhanul Islam <73143090+rayhanulmukul@users.noreply.github.com>
2025-04-08 06:31:53 +0600

    Leetcode

--- competitive-programming/1.cpp
+++ competitive-programming/1.cpp
@@ -1,64 +1,16 @@
-/*بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيمِ*/
-#include "ext/pb_ds/assoc_container.hpp"
-#include "ext/pb_ds/tree_policy.hpp"
-#include <bits/stdc++.h>
-using namespace std;
-using namespace __gnu_pbds;
-
-#define ll long long int
-#define int ll
-#define ld long double
-#define pb push_back
-#define ft front()
-#define bk back()
-#define pi 2*acos(0.0)
-#define gap ' '
-#define en '\n'
-#define endl en
-#define mem(a, b) memset(a, b, sizeof(a))
-#ifdef TESLA
-#include "main.hpp"
-#else
-#define dbg(...)
-#endif
-mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
-#define rng(x,y) uniform_int_distribution<int>(x,y)(rng)
-#define F0R(i,a,b) for (int i = (a); i < (b); ++i)
-#define FOR(i,a) F0R(i,0,a)
-const int MOD = 1e9+7;
-const int MAX = 2e5+5;
-const int INF = 1e18;
-int dx[] = {0, 0, +1, -1, -1, +1, -1, +1};
-int dy[] = {+1, -1, 0, 0, +1, +1, -1, -1};
-
-void solve(int tt){
-    int x;
-    cin >> x;
-
-    int y = 1;
-    while(y * 2 < x){
-        y *= 2;
-    }
-    for (int it : {y, y - 1}) {
-        if (it >= 1 && it < x) {
-            int z = x ^ it;
-            if (x + it > z && it + z > x && x + z > it) {
-                cout << it << endl;
-                return;
+class Solution {
+    public:
+        int minimumOperations(vector<int>& nums) {
+            int sz = nums.size();
+            unordered_map <int, int> freq;
+            int i = 0;
+            for(i = sz - 1; i >= 0; i--){
+                if(freq[nums[i]]){
+                    break;
+                }
+                freq[nums[i]]++;
             }
+            i++;
+            return ceil(i / 3.0);
         }
-    }
-    cout << -1 << en;
-}
-int32_t main(){
-#ifndef DEBUG
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-#endif
-    int t = 1;
-    cin >> t;
-    for(int i = 1; i <= t; i++){
-        solve(i);
-    }
-    return 0;
-}
+    };
